de2820f4be58d86f3477db7376c3839bc2a077ff
Melvin Jose Johnson Premkumar <jmelvinjose73@yahoo.com>
2015-05-07 23:48:29 -0700

    Placed read relations into util

--- lua/util.lua
+++ lua/util.lua
@@ -48,4 +48,20 @@ function util.parseProcessedLine(line)
     return tokens, relations
 end
 
+-- A function to read the relations file and return a table from indices to relation strings
+function util.read_relations(path)
+   local path = path
+   local rel_file = io.open(path)
+   
+   local relations_table = {}
+   local i = 0
+   local line = rel_file:read("*l")
+   while line do
+      relations_table[i] = line
+      i = i+1
+      line = rel_file:read("*l")
+   end
+   return relations_table
+end
+
 return util
