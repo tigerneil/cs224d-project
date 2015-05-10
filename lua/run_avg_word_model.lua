4e3290e3ba2c3c6f5071af2c4f793bb048829194
ankurbpn <ankurbpn@stanford.edu>
2015-05-10 01:37:05 -0700

    filepath

--- lua/run_avg_word_model.lua
+++ lua/run_avg_word_model.lua
@@ -8,7 +8,7 @@ word_vec_file = "../data/glove/glove.6B.100d.txt"
 word_vec_model = "glove"
 
 relations_file = "../data/relations.txt"
-train_file = "../data/train/processed/train1m_processed.tsv"
+train_file = "../data/train/1m/train1m_processed.tsv"
 test_file = "../data/test_processed.tsv"
 output_file = "../data/test_out.txt"
 
