4c0ee29dd67113f393117852e9e01d12f15510fc
Melvin Jose Johnson Premkumar <jmelvinjose73@yahoo.com>
2015-05-10 17:35:33 -0700

    Adding results file. Some changes to evalute.py

--- python/evaluate.py
+++ python/evaluate.py
@@ -13,13 +13,21 @@ labelFile = open(args.labels, 'r')
 
 predictionFile = open(args.predictions, 'r')
 
+relStats = {}
+relCounts = {}
+
 numCorrect = 0
 total = 0
 for label, prediction in zip(labelFile, predictionFile):
     if label == prediction:
         numCorrect += 1
+        relStats[label] = relStats.get(label, 0) + 1
+    relCounts[label] = relCounts.get(label, 0) + 1
+    
     total += 1
 
 print numCorrect, total
 print 'Accuracy is: ', numCorrect/ float(total)
-
+print relCounts
+for k in relStats.keys():
+    print k, relStats[k]/float(relCounts[k])
