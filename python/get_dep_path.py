bcc6993ee320c2574ee0c45a25ee91d1cce81c1c
ankurbpn <ankurbpn@stanford.edu>
2015-05-29 11:48:18 -0700

    dependency path exdtraction updated

--- python/get_dep_path.py
+++ python/get_dep_path.py
@@ -233,10 +233,10 @@ def get_recurrent_features_new(row):
 
   m1 = mentions[0]
   m2 = mentions[1]
-  print row
+ # print row
   if m1 != m2:
 	link, path = ddlib.dep_path_between_words_new(word_obj_list, int(ends[0])-1, int(ends[1])-1)
-	feat = [m1["word"], m2["word"], m1["type"], m2["type"], path, link]
+	feat = [m1["mention_id"], m2["mention_id"], m1["type"], m2["type"], path, link]
 	if relation is not None:
 		feat.append(relation)
 	return feat
@@ -245,9 +245,10 @@ def get_recurrent_features_new(row):
 def get_basic_rnn_features(line):
 	lis = get_recurrent_features_new(line)
 	ret = []
-	ret.extend(lis[0][0])
+	ret.append(lis[0])
 	ret.extend(lis[5][1:-1])
-	ret.extend(lis[1][0])
+	ret.append(lis[1])
+	#ret.extend(lis[4])
 	rel = None
 	try:
 		rel = lis[6]
@@ -261,6 +262,7 @@ for line in sys.stdin:
 	dep_types.update(link)
  	if temp == None:
 		break
+	#print temp
 	out = " ".join(temp)
 	if rel is not None:
 		rel = rel[1:-1]
