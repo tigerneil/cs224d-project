a8d35cd6fd24138761329918aabbefc5b4460725
Mikhail Sushkov <sushkovm@gmail.com>
2015-05-30 16:55:00 -0700

    replace mention head with entity string after parse tree is generated

--- python/recursive/java/run_data_processing.sh
+++ python/recursive/java/run_data_processing.sh
@@ -11,13 +11,15 @@ export GSON_JAR=/Users/msushkov/Documents/gson-2.3.1.jar
 CP=$CORE_NLP_DIR/classes:$CORE_NLP_DIR/*:$GSON_JAR:.
 
 # if on corn
+JAVA_PREFIX=
 #JAVA_PREFIX=/afs/.ir/users/m/s/msushkov/jdk_linux_1.8.0_45/bin/
 
-$JAVA_PREFIXjavac -classpath $CP ProcessData.java
+$JAVA_PREFIX"javac" -classpath $CP ProcessData.java
 
 CURR_FILE=../../../data/train/100k/train_100k.tsv
+#CURR_FILE=../../../data/train/train_5.tsv
 
 # if on corn
 #CURR_FILE=/afs/.ir.stanford.edu/users/m/s/msushkov/cs224d-project/data/train/100k/train_100k.tsv
 
-cat $CURR_FILE | python ../process_kbp_data.py | $JAVA_PREFIXjava -classpath $CP ProcessData > out.txt 
+head -100 $CURR_FILE | python ../process_kbp_data.py | $JAVA_PREFIX"java" -classpath $CP ProcessData > out.txt 
